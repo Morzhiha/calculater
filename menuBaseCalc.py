72bff30e82eda487ff3ceb9ff460c4c2a1960e82
unknown <roza120298@mail.ru>
2019-08-02 13:52:54 +0400

    change gitignore

--- menuBaseCalc.py
+++ menuBaseCalc.py
@@ -6,6 +6,9 @@ class MenuBaseCalc:
     def __init__(self):
         self.calculator = Calculator()
 
+        self.operatorsList = {'+': self.calculator.sum, '-': self.calculator.sub,
+                         '*': self.calculator.multi, '/': self.calculator.div}
+
     def mainMenu(self):
         flag = True
         while flag:
@@ -19,25 +22,19 @@ class MenuBaseCalc:
         a = input('Первое число ')
         a = self.isNumber(a)
         operator = input('Введите одно из действий: +, -, *, /: ')
-        while operator not in ('+', '-', '*', '/'):
-            operator = input('Неизвестная операция! Введите ещё раз одно из действий: +, -, *, /: ')
+        self.isOperator(operator)
         b = input('Второе число ')
         b = self.isNumber(b)
         if operator == '/':
             while b == 0:
                 b = input('Делить на ноль нельзя! Введите ещё раз второе число ')
-        return int(a), operator, int(b)
+        return a, operator, b
 
     def getResult(self, a, operator, b):
-
-        operatorsList = {'+': self.calculator.sum(a, b), '-': self.calculator.sub(a, b),
-                        '*': self.calculator.multi(a, b), '/': self.calculator.div(a, b)}
-        print(operatorsList)
-        return operatorsList[operator]
+        return self.operatorsList[operator](a, b)
 
     def answer(self, s):
-        a, operator, b = s
-        print('{} {} {} = {}'.format(a, operator, b, self.getResult(a, operator, b)))
+        print('{} {} {} = {}'.format(s[0], s[1], s[2], self.getResult(s[0], s[1], s[2])))
 
     def isNumber(self, a):
 
@@ -45,3 +42,7 @@ class MenuBaseCalc:
             a = input('Некорректный формат данных! Введите ещё раз число ')
 
         return int(a)
+
+    def isOperator(self, operator):
+        while operator not in ('+', '-', '*', '/'):
+            operator = input('Неизвестная операция! Введите ещё раз одно из действий: +, -, *, /: ')
