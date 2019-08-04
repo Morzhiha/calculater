7113390d85ff9cf9910ad05f899601630205a25c
unknown <roza120298@mail.ru>
2019-08-04 10:01:36 +0400

    create new classes

--- calc/complexCalc.py
+++ calc/complexCalc.py
@@ -2,14 +2,48 @@ from calc.calculator import Calculator
 from menu.menuBaseCalc import MenuBaseCalc
 
 
+class ComplexNumber:
+    def __init__(self, re, im):
+        self.re = re
+        self.im = im
+
+    def getRe(self):
+        return self.re
+
+    def getIm(self):
+        return self.im
+
+
+class MethodsComplexCalc:
+
+    def __init__(self):
+        self.calculator = Calculator()
+
+    def sum(self, z1, z2):
+        return ComplexNumber(z1.getRe() + z2.getRe(), z1.getIm() + z2.getIm())
+
+    def sub(self, z1, z2):
+        return ComplexNumber(z1.getRe() - z2.getRe(), z1.getIm() - z2.getIm())
+
+    def multi(self, z1, z2):
+        return ComplexNumber(z1.getRe()*z2.getRe() - z1.getIm()*z2.getIm(),
+                             z1.getRe()*z2.getIm() + z1.getIm()*z2.getRe())
+
+    def div(self, z1, z2):
+        d = z2.getRe()*z2.getRe() + z2.getIm()*z2.getIm()
+        return ComplexNumber((z1.getRe()*z2.getRe() + z1.getIm()*z2.getIm())/d,
+                             (z1.getIm()*z2.getRe() - z1.getRe()*z2.getIm())/d)
+
+
 class ComplexCalc(MenuBaseCalc):
 
     def __init__(self):
         super().__init__()
         self.calculator = Calculator()
 
-        self.methodsList = {'+': self.calculator.sum, '-': self.calculator.sub,
-                         '*': self.calculator.multi, '/': self.calculator.div}
+        self.methodsComplex = MethodsComplexCalc()
+        self.methodsComplList = {'+': self.methodsComplex.sum, '-': self.methodsComplex.sub,
+                         '*': self.methodsComplex.multi, '/': self.methodsComplex.div}
 
     def mainMenu(self):
         flag = True
@@ -23,6 +57,7 @@ class ComplexCalc(MenuBaseCalc):
     def getComplex(self):
         a, b = input('Введите действительную и мнимую части первого числа ').split()
         z1 = self.isComplexNumber(a, b)
+
         operator = input('Введите одно из действий: +, -, *, /: ')
         self.isOperator(operator)
         a, b = input('Введите действительную и мнимую части второго числа ').split()
@@ -32,18 +67,17 @@ class ComplexCalc(MenuBaseCalc):
         return z1, operator, z2
 
     def answer(self, numbers):
-        a, operator, b = numbers
-        print('{} {} {} = {}'.format(a, operator, b, self.getResult(a, operator, b)))
+        z1, operator, z2 = numbers
+        print('({}+{}i) {} ({}+{}i) = {}'.format(z1.getRe(), z1.getIm(), operator, z2.getRe(), z2.getIm(),
+                                     self.getResult(z1, operator, z2)))
 
     def isComplexNumber(self, num1, num2):
         while not str(num1).isdigit() and str(num2).isdigit():
             num1, num2 = input('Некорректный формат данных! Введите ещё раз действительную и мнимую части ').split()
-        return complex(int(num1), int(num2))
+        return ComplexNumber(int(num1), int(num2))
 
     def getResult(self, a, operator, b):
-        return self.methodsList[operator](a, b)
+        z = self.methodsComplList[operator](a, b)
+        return '(' + str(z.getRe()) + '+' + str(z.getIm()) + 'i)'
 
 
-    # def isOperator(self, operator):
-    #     while operator not in self.methodsList.keys():
-    #         operator = input('Неизвестная операция! Введите ещё раз одно из действий: +, -, *, /: ')
